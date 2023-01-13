60bfffb9147cea6237296d93eeb1c87b3ce8d8c7
Felipe <zfelipesilveira@gmail.com>
2023-01-13 13:22:10 -0300

    att

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -13,6 +13,7 @@ class CalcController {
         
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -33,6 +34,12 @@ class CalcController {
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach((btn, index) => {
+            btn.addEventListener('click', e=> {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });    
+        })
     }
 
     setDisplayDateTime(){
